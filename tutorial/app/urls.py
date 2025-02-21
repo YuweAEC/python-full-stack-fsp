bd581d8ae9a704dcf525258af7cd862ef0c5657c
Yuvraj Singh <yuwe18b@gmail.com>
2025-02-21 15:16:21 +0530

    add code im files

--- tutorial/app/urls.py
+++ tutorial/app/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .import views
 
 urlpatterns=[
-    path("index",views.index,name="index"),
-    
+    path("",views.index,name="index"),
+    path('new',views.data_time_view,name='new'),
 ]
--- tutorial/app/views.py
+++ tutorial/app/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
+import django.http import HttpResponse
+import datetime
 
 # Create your views here.
 
-from django.http import HttpResponse
-
 def index(request):
     return HttpResponse("<h1>Hello worl, my first django project")
+
+def new(request):
+    data=datetime.datetime.now()
+    h=int(date.strftime("%H"))
+
+    if h<12:
+        msg = "hello everyone ...! good morning tag"
+    elif h<16:
+        msg = "hello everyone...! good afternoon"
+    elif h<21:
+        msg = "hello everyone...! good evening"
+    else:
+        msg = "hello everyone...! time to sleep good night"
+
+    return render(request,"app/home.html")
